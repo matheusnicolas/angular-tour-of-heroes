bf39bfd9dde914d2af2995c3e9a20e8ea4bc4dd8
matheus.nicolas <matheus.nicolas@virtus.ufcg.edu.br>
2019-07-09 20:55:21 -0300

    hero-detail component and hero-service added

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service'
 
 @Component({
   selector: 'app-heroes',
@@ -14,12 +14,18 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: 'Windstorm'
   }
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void{
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes)
   }
 
   onSelect(hero: Hero): void {
